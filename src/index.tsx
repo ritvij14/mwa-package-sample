a130fdf1f8adf0f20105cbf5d4741fcc738ebb9a
Ritvij Kumar Sharma <ritvij2001@gmail.com>
2023-05-03 10:24:10 +0530

    :ambulance: removed nativewind/tailwind and replaced with nativebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,24 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
+import React from 'react';
+import { NativeBaseProvider, Box } from 'native-base';
+
+export const ExampleComponent: React.FC = () => {
+  return (
+    <Box>
+      <Box
+        alignSelf="center" // bg="primary.500"
+        marginTop="50%"
+        _text={{
+          fontSize: 'md',
+          fontWeight: 'medium',
+          color: 'warmGray.50',
+          letterSpacing: 'lg',
+        }}
+        bg={['red.400', 'blue.400']}
+      >
+        This is a Box
+      </Box>
+    </Box>
+  );
+};
+
+export { NativeBaseProvider };
